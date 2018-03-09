068028a268fd9ed4e17f68e94db8e650c4634997
William Stevens <contact@wastevensv.com>
2018-03-09 03:44:29 -0500

    Moved to module. Wrote start of test. Added get_dev.

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,57 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![feature(libc)]
 
+#[allow(dead_code)]
 include!(concat!(env!("OUT_DIR"), "/xwiimote.rs"));
 
-struct Wiimote {
-    dev: xwii_iface,
-}
+pub mod riimote {
+    use super::*;
+    
+    use std::ffi::CStr;
+    use std::str;
 
-impl Wiimote {
-    fn new(syspath: *const i8) -> Self {
-        let mut dev: xwii_iface = xwii_iface{_unused:[]};
-        unsafe {
-            let mut dev: *mut xwii_iface = &mut dev as *mut xwii_iface;
-            let dev: *mut *mut xwii_iface = &mut dev as *mut *mut xwii_iface;
-            let ret = xwii_iface_new(dev, syspath);
-            assert!(ret == 0,"xwii_iface_new Error: {}",ret);
 
-            let ret = xwii_iface_open(*dev, xwii_iface_available(*dev) |
-            xwii_iface_type_XWII_IFACE_WRITABLE);
-            assert!(ret == 0,"xwii_iface_open Error: {}",ret);
+    pub struct Wiimote {
+        dev: xwii_iface,
+    }
 
-            let ret = xwii_iface_watch(*dev, true);
-            assert!(ret == 0,"xwii_iface_watch Error: {}",ret);
+    impl Wiimote {
+        pub fn new() -> Option<Self> {
+            let mut path: Option<String> = None;
+            unsafe {
+                let mon: *mut xwii_monitor = xwii_monitor_new(false, false);
+                assert!(!mon.is_null() ,"xwii_monitor_new failed.");
+                let ent = xwii_monitor_poll(mon);
+                if !ent.is_null() {
+                    let c_str: &CStr = CStr::from_ptr(ent);
+                    let str_slice: &str = c_str.to_str().unwrap();
+                    path = Some(str_slice.to_owned());
+                }
+                xwii_monitor_unref(mon);
+            }
+            match path {
+                Some(path) => Some(Wiimote::new_from_path(path)),
+                None => None,
+            }
         }
-        return Wiimote{dev:dev}
+        pub fn new_from_path(syspath: String) -> Self {
+            let mut dev: xwii_iface = xwii_iface{_unused:[]};
+            unsafe {
+                let mut dev: *mut xwii_iface = &mut dev as *mut xwii_iface;
+                let dev: *mut *mut xwii_iface = &mut dev as *mut *mut xwii_iface;
+                let ret = xwii_iface_new(dev, syspath.as_ptr() as *const i8);
+                assert!(ret == 0,"xwii_iface_new Error: {}",ret);
+
+                let ret = xwii_iface_open(*dev, xwii_iface_available(*dev) |
+                                          xwii_iface_type_XWII_IFACE_WRITABLE);
+                assert!(ret == 0,"xwii_iface_open Error: {}",ret);
+
+                let ret = xwii_iface_watch(*dev, true);
+                assert!(ret == 0,"xwii_iface_watch Error: {}",ret);
+            }
+            return Wiimote{dev:dev}
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,11 @@
+extern crate riimote_sys;
+
+use riimote_sys::riimote::Wiimote;
+
 fn main() {
+    if let Some(wmote) = Wiimote::new() {
+        println!("WiiMote found!");
+    } else {
+        println!("No WiiMote found!");
+    };
 }
